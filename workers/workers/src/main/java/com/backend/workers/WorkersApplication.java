96a5d72e8d37f01727ecd4e7e125d37a0a6e1c55
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-10-27 13:57:43 +0300

    setup

---
+++ workers/workers/src/main/java/com/backend/workers/WorkersApplication.java
@@ -0,0 +1,13 @@
+package com.backend.workers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkersApplication.class, args);
+	}
+
+}
---
+++ workers/workers/src/test/java/com/backend/workers/WorkersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.workers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
