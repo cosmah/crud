e04e35ad47d68af61df72edd41ccece012e91cd4
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-10-30 14:20:29 +0300

    added employee services

---
+++ workers/src/main/java/com/backend/workers/service/EmployeeService.java
@@ -0,0 +1,45 @@
+package com.backend.workers.service;
+
+import com.backend.workers.exceptions.UserNotFoundException;
+import com.backend.workers.model.Employee;
+import com.backend.workers.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    //add it as constructor
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees(){
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id){
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by id" + id + " was not found"));
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
+    }
+
+
+}
